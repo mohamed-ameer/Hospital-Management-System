af3ce6f195bb9717d59366bf5fe87f02961f8da6
mohamed ameer <mohamedameer577@gmail.com>
2023-10-12 13:57:40 +0300

    Install Laravel 10 & All Packages & Preparing the Dashboard

---
+++ routes/dashboard.php
@@ -0,0 +1,5 @@
+<?php
+use App\Http\Controllers\Dashboard\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',[DashboardController::class, 'index'])->name('dashboard.index');
